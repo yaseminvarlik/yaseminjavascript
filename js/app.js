4ccdaf401349642b317a7d1a8dc7cf7814c46016
Yasemin Varlik <varly065@newschool.edu>
2018-09-04 15:12:27 -0400

    fix js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'Yasemin Varlik is a senior in CD major and a photography minor at Parsons The New School for Design.This is her portfolio for Web Advanced: Javascript, where she will be storing all her work through out the semester of Fall 2018.';
+var txt = 'Yasemin Varlik is a senior in CD major and a photography minor student at Parsons The New School for Design.This is her portfolio for Web Advanced: Javascript, where she will be storing all her work through out the semester of Fall 2018.';
 var speed = 30;
 
 window.onload = function typeWriter() {
