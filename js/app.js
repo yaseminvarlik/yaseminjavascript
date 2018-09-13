3430b6375221f2332e7241ba9ecb3b0373d3b223
Yasemin Varlik <varly065@newschool.edu>
2018-09-13 11:11:56 -0400

    add loop

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var i = 0;
-var txt = 'Yasemin Varlik is a senior in Communication Design and has a minor in Photography at Parsons The New School for Design.This is her portfolio for Web Advanced: Javascript, where she will be storing all her work through out the semester of Fall 2018.';
+var txt = `Yasemin Varlik is a senior in Communication Design and has a minor in Photography at Parsons The New School for Design.This is her portfolio for Web Advanced: Javascript, where she will be storing all her work through out the semester of Fall 2018.
+    `;
 var speed = 30;
 
 window.onload = function typeWriter() {
@@ -9,4 +10,12 @@ window.onload = function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+// var element = document.createElement("div");
+// 		element.className = "newHeader";
+// 		element.innerHTML = `
+// 		<h1>Header</h1>
+// 		<h2> header 2</h2>
+// 		`	
+// 		document.getElementById("wrapper").appendChild(element);
 
---
+++ js/loop.js
@@ -0,0 +1,14 @@
+// var myArray = ["Hello", "my", "name", "is", "", ".", "I", "am", "from", "", "."];
+// var i = 4;
+// var len = myArray.length;
+// var txt = "";
+
+// $(document).ready(function() {
+
+	
+// 	for (var i = 0; i<len ; i++) {
+
+// 			txt += myArray[i] + "<br>";
+// 			}
+// 			document.getElementById("repeat").innerHTML = txt; 
+// });
